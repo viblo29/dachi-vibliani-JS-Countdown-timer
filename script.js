5fbf66ff5f0c11b38c939d062c830296920426bf
viblo29 <dachivibliani29@gmail.com>
2024-12-25 14:45:41 +0400

    Added flip animation

--- script.js
+++ script.js
@@ -1,28 +1,94 @@
-let totalSeconds
-
-
-
-function updateCountdown() {
-    // MM/DD/YYYY
-    totalSeconds = Math.floor((new Date('01/01/2025') - new Date()) / 1000);
-    if (totalSeconds <= 0) {
-        document.getElementById("countdown").innerHTML = "<h2>Time's up!</h2>";
-        clearInterval(intervalId);
-        return;
+window.addEventListener('load', () => {
+    const days = document.querySelector('.days')
+    const hours = document.querySelector('.hours')
+    const minutes = document.querySelector('.minutes')
+    const seconds = document.querySelector('.seconds')
+    
+    let timeLeft = {
+        d: 0,
+        h: 0,
+        m: 0,
+        s: 0,
     }
-    setTimeLeft()
-
-    document.getElementById("days").textContent = days.toString().padStart(2, "0");
-    document.getElementById("hours").textContent = hours.toString().padStart(2, "0");
-    document.getElementById("minutes").textContent = minutes.toString().padStart(2, "0");
-    document.getElementById("seconds").textContent = seconds.toString().padStart(2, "0");
-}
-function setTimeLeft() {
-    days = Math.floor(totalSeconds / (60 * 60 * 24));
-    hours = Math.floor(totalSeconds / (60 * 60) % 24);
-    minutes = Math.floor(totalSeconds / (60) % 60);
-    seconds = Math.floor(totalSeconds % 60);
-}
-
-const intervalId = setInterval(updateCountdown, 1000);
-updateCountdown();
+    
+    let totalSeconds;
+    
+    function init() {
+        totalSeconds = Math.floor((new Date('01/01/2025') - new Date()) / 1000); 
+        setTimeLeft();
+        let interval = setInterval(() => {
+            if (totalSeconds < 0) {
+                clearInterval(interval);
+            }
+            countTime();
+        }, 1000);
+    }
+    
+    function countTime() {
+        if (totalSeconds > 0) {
+            --timeLeft.s;
+            if (timeLeft.m >= 0 && timeLeft.s < 0) {
+                timeLeft.s = 59;
+                --timeLeft.m;
+                if (timeLeft.h >= 0 && timeLeft.m < 0) {
+                    timeLeft.m = 59;
+                    --timeLeft.h;
+                    if (timeLeft.d >= 0 && timeLeft.h < 0) {
+                        timeLeft.h = 23;
+                        --timeLeft.d;
+                    }
+                }
+            }
+        }
+        --totalSeconds;
+        printTime();
+    }
+    
+    function printTime() {
+        animateFlip(days, timeLeft.d);
+        animateFlip(hours, timeLeft.h);
+        animateFlip(minutes, timeLeft.m);
+        animateFlip(seconds, timeLeft.s);
+    }
+    
+    function animateFlip(element, value) {
+        const valueInDom = element.querySelector('.bottom-back').innerText;
+        const currentValue = value < 10 ? '0' + value : '' + value;
+    
+        if (valueInDom === currentValue) return;
+    
+        element.querySelector('.top-back span').innerText = currentValue;
+        element.querySelector('.bottom-back span').innerText = currentValue;
+    
+    
+        gsap.to(element.querySelector('.top'), 0.7, {
+            rotationX: '-180deg',
+            transformPerspective: 300,
+            ease: Quart.easeOut,
+            onComplete: function() {
+                element.querySelector('.top').innerText = currentValue; 
+                element.querySelector('.bottom').innerText = currentValue; 
+                gsap.set(element.querySelector('.top'), {rotationX: 0});
+            }
+        });
+    
+        gsap.to(element.querySelector('.top-back'), 0.7, {
+            rotationX: 0,
+            transformPerspective: 300,
+            ease: Quart.easeOut,
+            clearProps: 'all'
+        });
+    
+    }
+    
+    
+    
+    function setTimeLeft() {
+        timeLeft.d = Math.floor(totalSeconds / (60 * 60 * 24));
+        timeLeft.h = Math.floor(totalSeconds / (60 * 60) % 24);
+        timeLeft.m = Math.floor(totalSeconds / (60) % 60);
+        timeLeft.s = Math.floor(totalSeconds % 60);
+    }
+    
+    init();
+});
